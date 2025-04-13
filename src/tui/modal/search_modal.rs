b65fcb7e0b408f813b60830bb5c29be4cac8074e
Zuruuh <zuruh.dev@proton.me>
2025-04-13 17:09:45 +0000

    Function preview

--- src/tui/modal/search_modal.rs
+++ src/tui/modal/search_modal.rs
@@ -1,8 +1,10 @@
 use crossterm::event::{KeyCode, KeyEvent};
 use ratatui::{
     buffer::Buffer,
-    layout::{Constraint, Layout, Rect},
-    widgets::{Block, List, ListItem, ListState, Paragraph, StatefulWidget, Widget},
+    layout::{Constraint, Layout, Margin, Rect},
+    style::{Color, Style, Stylize},
+    text::{Line, Span},
+    widgets::{Block, List, ListItem, ListState, Padding, Paragraph, StatefulWidget, Widget, Wrap},
 };
 use tui_input::Input;
 
@@ -17,6 +19,7 @@ use super::Modal;
 pub struct SearchModal {
     r#type: SearchModalType,
     query: Input,
+    selection_offset: usize,
 }
 
 impl SearchModal {
@@ -24,6 +27,7 @@ impl SearchModal {
         Modal::SearchModal(Self {
             r#type,
             query: Input::default(),
+            selection_offset: 0,
         })
     }
 
@@ -39,6 +43,21 @@ pub enum SearchModalType {
 
 impl EventHandler for SearchModal {
     async fn on_key_event(&mut self, key: &KeyEvent) -> EventHandlerResult {
+        match key.code {
+            KeyCode::Down => {
+                self.selection_offset += 1;
+
+                return EventHandlerResult::Handled;
+            }
+            KeyCode::Up => {
+                if self.selection_offset > 0 {
+                    self.selection_offset -= 1;
+                }
+                return EventHandlerResult::Handled;
+            }
+            _ => {}
+        };
+
         use tui_input::backend::crossterm::EventHandler as _;
         match self
             .query
@@ -61,7 +80,7 @@ impl StatefulWidget for &mut SearchModal {
         };
 
         let [list_area, preview_area] =
-            Layout::horizontal([Constraint::Fill(1), Constraint::Fill(1)])
+            Layout::horizontal([Constraint::Ratio(1, 3), Constraint::Ratio(2, 3)])
                 .split(preview_and_list_area)[..]
         else {
             unreachable!()
@@ -77,30 +96,48 @@ impl StatefulWidget for &mut SearchModal {
                 Function::Definition(function_definition) => Some(function_definition),
                 Function::Alias(_) => None,
             })
-            .map(|function| format!("{}(...)", function.name))
-            .filter(|name| {
+            .filter(|function| {
                 self.query
                     .value()
                     .split(' ')
-                    .all(|word| name.contains(word))
+                    .all(|word| function.name.contains(word))
             })
-            .map(ListItem::new);
+            .collect::<Vec<_>>();
+
+        if self.selection_offset >= items.len() {
+            self.selection_offset = items.len().checked_sub(1).unwrap_or_default()
+        }
+
+        let selected_function = items
+            .iter()
+            .enumerate()
+            .find(|(i, _)| *i == self.selection_offset)
+            .map(|(_, function)| *function)
+            .cloned();
+
+        let items = items.into_iter().enumerate().map(|(i, function)| {
+            if i == self.selection_offset {
+                ListItem::new(Line::from(vec![
+                    Span::styled("> ", Style::default().fg(Color::LightRed)),
+                    Span::styled(format!("{}(...)", function.name), Style::default().italic()),
+                ]))
+                .style(Style::default().bg(Color::DarkGray))
+            } else {
+                ListItem::new(format!("  {}(...)", function.name))
+            }
+        });
 
         let list = List::new(items);
         StatefulWidget::render(list, list_area, buf, &mut ListState::default());
 
-        // Paragraph::new(
-        //     shared_state
-        //         .parsed_files_snapshot
-        //         .iter()
-        //         .filter_map(|function| match function {
-        //             Function::Definition(def) => Some(def),
-        //             _ => None,
-        //         })
-        //         .last()
-        //         .map(ToString::to_string)
-        //         .unwrap_or_default(),
-        // )
-        // .render(area, buf);
+        let preview = Paragraph::new(
+            selected_function
+                .map(|function| function.to_string())
+                .unwrap_or("No preview available".to_string()),
+        )
+        .wrap(Wrap::default())
+        .block(Block::bordered().padding(Padding::horizontal(1)));
+
+        preview.render(preview_area.inner(Margin::new(1, 0)), buf);
     }
 }
