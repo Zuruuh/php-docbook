87a8c8db0fe6ddcbca8e0ab1c838a56966e7fe58
Zuruuh <zuruh.dev@proton.me>
2025-04-13 16:42:57 +0000

    Search works

--- src/tui/modal/search_modal.rs
+++ src/tui/modal/search_modal.rs
@@ -2,11 +2,14 @@ use crossterm::event::{KeyCode, KeyEvent};
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Layout, Rect},
-    widgets::{Block, Paragraph, StatefulWidget, Widget},
+    widgets::{Block, List, ListItem, ListState, Paragraph, StatefulWidget, Widget},
 };
 use tui_input::Input;
 
-use crate::tui::{EventHandler, EventHandlerResult, SharedState};
+use crate::{
+    parser::function::Function,
+    tui::{EventHandler, EventHandlerResult, SharedState},
+};
 
 use super::Modal;
 
@@ -51,14 +54,40 @@ impl StatefulWidget for &mut SearchModal {
     type State = SharedState;
 
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
-        let [input_area, preview_area] =
+        let [input_area, preview_and_list_area] =
             Layout::vertical([Constraint::Length(3), Constraint::Fill(1)]).split(area)[..]
+        else {
+            unreachable!()
+        };
+
+        let [list_area, preview_area] =
+            Layout::horizontal([Constraint::Fill(1), Constraint::Fill(1)])
+                .split(preview_and_list_area)[..]
         else {
             unreachable!()
         };
 
         let input_widget = Paragraph::new(self.query.value()).block(Block::bordered());
         input_widget.render(input_area, buf);
+
+        let items = state
+            .parsed_files_snapshot
+            .iter()
+            .filter_map(|function| match function {
+                Function::Definition(function_definition) => Some(function_definition),
+                Function::Alias(_) => None,
+            })
+            .map(|function| format!("{}(...)", function.name))
+            .filter(|name| {
+                self.query
+                    .value()
+                    .split(' ')
+                    .all(|word| name.contains(word))
+            })
+            .map(ListItem::new);
+
+        let list = List::new(items);
+        StatefulWidget::render(list, list_area, buf, &mut ListState::default());
 
         // Paragraph::new(
         //     shared_state
